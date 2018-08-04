c84e08d1dd40be5fbac6df3003772ed8d8478df7
Thirsty Fermat <frescura.peter@gmail.com>
2018-08-04 05:45:16 +0200

    added go routine to check for pulse

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"time"
 
 	eeureka "github.com/puzzledvacuum/backing-fulfillment/eeureka"
 	service "github.com/puzzledvacuum/backing-fulfillment/service"
@@ -14,6 +16,30 @@ func main() {
 	}
 
 	eeureka.RegisterAt("http://localhost:8081", "backing-fulfillment", port, "8443")
+
+	ticker := time.NewTicker(500 * time.Millisecond)
+	count := 1
+
+	for t := range ticker.C {
+		frontend, err := eeureka.GetServiceInstances("backing-catalog")
+		if count%4 == 0 {
+			fmt.Println("...Waiting for message:", t)
+		}
+		if err != nil {
+			fmt.Printf("Service not available, %v\n", err)
+			// return
+		}
+		if len(frontend) > 0 {
+			fmt.Println("HostName:", frontend[0].HostName)
+			fmt.Println("Port:", frontend[0].Port.Port)
+			break
+		}
+		count++
+		// if count%60 == 0 {
+		// 	break
+		// }
+	}
+
 	// Ordinarily we'd use a CF environment here, but we don't need it for
 	// the fake data we're returning.
 	server := service.NewServer()
